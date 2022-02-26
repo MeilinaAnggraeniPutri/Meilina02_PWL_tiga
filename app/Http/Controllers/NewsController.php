9bb8ba3cf99421927c35b282edde955a96a5ee35
Meilina Anggraeni Putri <meilinaanggraeniputri@gmail.com>
2022-02-26 07:07:19 -0800

    Meilina02_PWL_tiga

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function news($berita){
+        return 'Halaman ini menampilkan berita seputar '.$berita;
+    }
+}
+ 
